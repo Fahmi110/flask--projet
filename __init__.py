27c5d48cb171ef0498c6997fba1b863c7299777a
fahmi idriss <issa110@hotmail.com>
2023-12-06 17:56:56 +0100

    premier pas

---
+++ __init__.py
@@ -0,0 +1,22 @@
+
+from flask import Flask
+
+app=Flask(__name__)
+
+
+
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
+
+
+
+
+if __name__ =='__main__':
+    app.run(debug= True )
--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from flask import Flask
 
 app=Flask(__name__)
 
-from flask import render_template
+from flask import render_template,redirect
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-
 
 
 
@@ -24,7 +21,17 @@ def propos():
     return render_template('propos.html'  )
 
 
+@app.route('/sign up')
+def formation():
+    return render_template('sign up.html'  )
 
+@app.route('/user')
+def user():
+    return render_template('user.html'  )
+
+# @app.route('/user')
+# def layout():
+#     return render_template('user.html'  )
 
 if __name__ =='__main__':
     app.run(debug= True )
