aa99a7472b6da3ade30fcc90aeba18e7df3f67b1
fahmi idriss <issa110@hotmail.com>
2024-01-01 18:02:32 +0100

    modification

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask
 
 app=Flask(__name__)
 
-from flask import render_template,redirect
+from flask import render_template
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 
 @app.route('/contact')
